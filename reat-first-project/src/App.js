6d25508ab2456215eb440254cbb4b773b0dd036d
Prathamesh Tjorat <thoratprathamesh2@gmail.com>
2023-11-27 10:32:11 +0530

    Update

--- reat-first-project/src/App.js
+++ reat-first-project/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+
 function App(){
   return(
     <>    
     {/* <Navbar title="TextUtils2" aboutus = "About1" /> */}
-    <Navbar />
+    <Navbar title='TextUtils'/>
+
+    <div className='container my-3'>
+
+    <TextForm heading="Enter the text to analyze below."/>
+    </div>
     </>
 
   )
